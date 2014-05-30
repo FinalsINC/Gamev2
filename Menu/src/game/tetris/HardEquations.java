7fa07d988b3ab49a44651b9fa8df86ef025ef7ca
atpearson <definitelymabe@hotmail.com>
2014-05-30 15:57:27 -0700

    Create HardEquations.java

---
+++ Menu/src/game/tetris/HardEquations.java
@@ -0,0 +1,44 @@
+package game.tetris;
+
+import java.util.Random;
+
+public class HardEquations implements EquationGenerator {
+
+	public Equation newEquation() {
+		int first = 0, second = 0, op = 0;
+		Random rand = new Random();
+		do {
+			first = rand.nextInt(100);
+			second = rand.nextInt(100);
+			op = rand.nextInt(2);
+		} while (checkValidity(first, second, op) == false);
+		return new Equation(first, second, op);
+	}
+
+	private boolean checkValidity(int first, int second, int op) {
+		switch (op) {
+		case 0:
+			if (first + second >= 100)
+				return false;
+			else
+				return true;
+		case 1:
+			if (first - second < 0 || first > second)
+				return false;
+			else
+				return true;
+		case 2:
+			if (first * second >= 100)
+				return false;
+			else
+				return true;
+		case 3:
+			if (first % second != 0)
+				return false;
+			else
+				return true;
+		default:
+			return false;
+		}
+	}
+}
