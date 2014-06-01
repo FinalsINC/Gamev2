215951b406c5c135f7b1071dc2b4c97cf1c3c263
atpearson <definitelymabe@hotmail.com>
2014-06-01 16:23:40 -0700

    Update HardEquations.java

--- Menu/src/game/tetris/HardEquations.java
+++ Menu/src/game/tetris/HardEquations.java
@@ -9,7 +9,7 @@ public class HardEquations implements EquationGenerator {
 		Random rand = new Random(System.currentTimeMillis());
 		do {
 			first = rand.nextInt(90)+10;
-			second = rand.nextInt(10);
+			second = rand.nextInt(9)+1;
 			op = rand.nextInt(4);
 		} while (checkValidity(first, second, op) == false);
 		return new Equation(first, second, op);
@@ -23,7 +23,7 @@ public class HardEquations implements EquationGenerator {
 			else
 				return true;
 		case 1:
-			if (first - second < 0 || first > second)
+			if (first < second)
 				return false;
 			else
 				return true;
